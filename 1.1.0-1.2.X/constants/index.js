d0731b4349a5262c2fad58e7f1d20a985224fc62
wangkai1995 <wwwwkwy@126.com>
2020-06-09 11:39:20 +0800

    delete save file

--- 1.1.0-1.2.X/constants/index.js
+++
@@ -1,4 +0,0 @@
-//上报数据类型
-export const reportType = ['unKownType', 'log', 'network', 'vue'];
-//版本号
-export const version = '1.1.0';
--- 1.1.0-1.2.X/services/observer/vue/defaultConfig.js
+++
@@ -1,8 +0,0 @@
-/*
- 
- 	observer vue 实例默认配置数据
- */
-export default {
-    //是否启动性能分析   暂时未做
-    // 	isPerformance:true,
-}
